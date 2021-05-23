80656681a0d0c84b7f7f181ca693be88430922e3
Artur Schaefer <artur.schaefer@picpay.com>
2021-05-23 12:24:35 -0300

    feat: create remaining repositories

--- app/src/main/java/com/schaefer/architectureplayground/repository/EpisodesRepository.kt
+++ app/src/main/java/com/schaefer/architectureplayground/repository/EpisodesRepository.kt
@@ -1,16 +1,16 @@
 package com.schaefer.architectureplayground.repository
 
-import com.schaefer.architectureplayground.model.Character
+import com.schaefer.architectureplayground.model.Episode
 import com.schaefer.architectureplayground.model.PagedResult
 import com.schaefer.architectureplayground.network.ResultWrapper
 import com.schaefer.architectureplayground.network.RickAndMortyApiDataSource
 import com.schaefer.architectureplayground.network.safeApiCall
 import javax.inject.Inject
 
-class CharactersRepository @Inject constructor(
+class EpisodesRepository @Inject constructor(
     private val dataSource: RickAndMortyApiDataSource
 ) {
-    suspend fun getCharacters(): ResultWrapper<PagedResult<Character>> {
-        return safeApiCall { dataSource.getCharacters() }
+    suspend fun getEpisodes(): ResultWrapper<PagedResult<Episode>> {
+        return safeApiCall { dataSource.getEpisodes() }
     }
 }

23a52468496548e90c999c4c54ead0ad025e34d9
Artur Schaefer <artur.schaefer@picpay.com>
2021-05-31 09:01:44 -0300

    feat: load status image inside adapter

--- app/src/main/java/com/schaefer/architectureplayground/ui/characters/CharactersAdapter.kt
+++ app/src/main/java/com/schaefer/architectureplayground/ui/characters/CharactersAdapter.kt
@@ -1,13 +1,16 @@
 package com.schaefer.architectureplayground.ui.characters
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.schaefer.architectureplayground.R
 import com.schaefer.architectureplayground.databinding.ItemCharacterBinding
 import com.schaefer.architectureplayground.model.Character
+import com.schaefer.architectureplayground.model.StatusCharacter
 
 class CharactersAdapter(diffUtil: DiffUtil.ItemCallback<Character>) :
     PagingDataAdapter<Character, CharactersAdapter.CharacterViewHolder>(diffUtil) {
@@ -30,8 +33,21 @@ class CharactersAdapter(diffUtil: DiffUtil.ItemCallback<Character>) :
                 holder.itemView.setOnClickListener {
                     // TODO
                 }
+
+                Glide.with(holder.itemView.context)
+                    .load(getImageAliveStatus(this?.status))
+                    .into(binding.ivLifeStatus)
             }
         }
+    }
+
+    fun getImageAliveStatus(statusCharacter: StatusCharacter?): Int{
+       return when (statusCharacter){
+            StatusCharacter.ALIVE -> R.drawable.ic_circle_green
+            StatusCharacter.DEAD -> R.drawable.ic_circle_red
+            StatusCharacter.UNKNOWN -> R.drawable.ic_unknown
+           else -> R.drawable.ic_unknown
+       }
     }
 
     inner class CharacterViewHolder(val binding: ItemCharacterBinding) :
